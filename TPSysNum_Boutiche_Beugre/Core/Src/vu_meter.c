79e770860ea5f6ad3c73cce1e3e477c045215cbf
AbdelBoutiche <abdel.boutiche@hotmail.com>
2023-11-09 17:25:07 +0100

    Realisation du TP jusqu'a la partie 4.4

---
+++ TPSysNum_Boutiche_Beugre/Core/Src/vu_meter.c
@@ -0,0 +1,23 @@
+/*
+ * vu_meter.c
+ *
+ *  Created on: Nov 9, 2023
+ *      Author: saiko no oniisan
+ */
+#include "vu_meter.h"
+
+#define VU_ADDR 0x40
+#define VU_IODIRA_ADDRESS 0x00
+#define VU_IODIRB_ADDRESS 0x01
+#define VU_GPIOA_ADDRESS 0x12
+#define VU_GPIOB_ADDRESS 0x13
+#define VU_PORTA 1
+#define VU_PORTB 0
+
+int vu_init(h_vu_t *h_vu){
+
+
+}
+int vu_led(h_vu_t *h_vu, uint8_t port, uint8_t led, uint8_t state){
+
+}
